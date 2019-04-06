7879ea8711ae99d24c7c86902e34300581633035
Alexander Tobek <sasha.torbek@yandex.ru>
2019-04-06 12:27:03 +0300

    add oImgs for view, save images in oImgs

--- src/index.js
+++ src/index.js
@@ -1,23 +1,41 @@
 import JSZip from 'jszip';
 const uploadFile = document.getElementById('uploadFile');
 const zip = new JSZip();
-const fileReader = new FileReader();
+// const fileReader = new FileReader();
 
 let unzipInObject = async (data) => {
+
+    const oImgs = {};
+    let id = 0;
+
     try {
-        let contents = await zip.loadAsync(data);
-        let files = contents.files;
+        const contents = await zip.loadAsync(data, { base64: true });
+        const files = contents.files;
 
         for (let file in files) {
-            if ( files.hasOwnProperty( file ) ) {
+            if (files.hasOwnProperty(file)) {
                 console.log(file);
+
+                let isIncludes = file.includes('expected') || file.includes('diff') || file.includes('json');
+
+                if (!oImgs.hasOwnProperty(file) && !isIncludes) {
+
+                    let nfile =file.substr(0, file.length-11);
+
+                    oImgs[id++] = { 'actual': files[file],
+                        'expected': files[`${nfile}-expected.jpg`],
+                        'diff': files[`${nfile}-diff.jpg`]
+                    }
+                }
             }
         }
     } catch (e) {
         console.log('Ошибка!');
     }
+
+    return oImgs;
 };
 
 uploadFile.addEventListener('change', e => {
-    unzipInObject(e.target.files[0]);
+    console.log(unzipInObject(e.target.files[0]));
 });
