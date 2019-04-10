2c2b58aa4d46cda427b15f7c7953acd0241ba5bd
Alexander Torbek <atorbek@naumen.ru>
2019-04-10 17:50:21 +0300

    add button save, add action on button save, save status image in oImgs

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import JSZip from 'jszip';
 import View from './view.js';
 import Images from './images.hbs';
 const uploadFile = document.getElementById('uploadFile');
-const zip = new JSZip();
+const jsZip = new JSZip();
+const oImgs = {
+    images: []
+};
 
-let unzipImgInObj = async (data) => {
-    const oImgs = {
-        images: []
-    };
+let unzipImgInObj = async (zip) => {
     let id = 0;
 
     try {
-        const contents = await zip.loadAsync(data);
+        const contents = await jsZip.loadAsync(zip);
         const files = contents.files;
 
         for (let file in files) {
             if (files.hasOwnProperty(file) && file.includes('json')) {
-
                 let text = await files[file].async('text');
                 let obj = JSON.parse(text);
                 const [actual, expected, diff] = [obj.actual, obj.expected, obj.diff];
@@ -25,7 +24,8 @@ let unzipImgInObj = async (data) => {
                     'id': id++,
                     'actual': await files[actual].async('base64'),
                     'expected': await files[expected].async('base64'),
-                    'diff': await files[diff].async('base64')
+                    'diff': await files[diff].async('base64'),
+                    'agreed': false
                 })
             }
         }
@@ -36,6 +36,33 @@ let unzipImgInObj = async (data) => {
     return oImgs;
 };
 
+let zipAgreedImgs = (data) => {
+    for (let img in data) {
+        if (data.hasOwnProperty(img) && img.agreed) {
+            // jsZip
+        }
+    }
+
+    console.log('It\'s work!');
+
+    return true;
+};
+
 uploadFile.addEventListener('change', async e => {
     View.render(Images, await unzipImgInObj(e.target.files[0]), 'body');
+});
+
+document.addEventListener('click', e => {
+    if (e.target.id === 'agreement') {
+        oImgs.images.forEach(elem => {
+            if (elem.id === +e.target.dataset.id) {
+                elem.agreed = true;
+            }
+        })
+    }
+
+    if (e.target.id === 'save') {
+        zipAgreedImgs(oImgs);
+    }
+
 });
